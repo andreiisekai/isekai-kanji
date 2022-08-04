5629102b2150facec0f610cc93a5c4790c7cf378
internetkun <internetkundev@gmail.com>
2022-08-04 18:36:08 +0300

    Kana System Implementation

---
+++ Isekai Kanji/Assets/Scripts/Kana.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Kana 
+{
+    [SerializeField] private string symbol;
+    [SerializeField] private string romaji;
+}
---
+++ Isekai Kanji/Assets/Scripts/KanaSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "KanaSystem", fileName = "KanaSystem")]
+public class KanaSystem : ScriptableObject
+{
+    public Kana[] kana;
+}
