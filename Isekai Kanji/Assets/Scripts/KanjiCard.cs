8264b4ffbc7f555046c6f57d734eb1075ebe730c
internetkun <internetkundev@gmail.com>
2022-08-17 20:54:53 +0300

    Kanji Card Canvas

---
+++ Isekai Kanji/Assets/Scripts/KanjiCard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KanjiCard : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI onyomiRomaji, onyomiKana, kunyomiRomaji, kunyomiKana, meaning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
